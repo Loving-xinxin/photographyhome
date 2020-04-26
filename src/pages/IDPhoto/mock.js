75dc87c734f8a86d3c7ccd4d9be3ff24ab5a4c24
Loving-xinxin <2603712235@qq.com>
2020-04-26 14:32:36 +0800

    update

--- src/pages/IDPhoto/mock.js
+++ src/pages/IDPhoto/mock.js
@@ -14,4 +14,9 @@ export const picShow = [
     img:
       'https://loving-xinxin.coding.net/p/sheyingtupian/d/sheyingtupian/git/raw/master/%E5%9B%BE%E7%89%87%E5%B1%95%E7%A4%BA%E6%A8%A1%E6%9D%BF%E5%9B%BE.jpg',
   },
+  {
+    id:'4',
+    img:'https://loving-xinxin.coding.net/p/sheyingtupian/d/sheyingtupian/git/raw/master/%E8%93%9D%E5%BA%95.jpg'
+  }
+
 ];
